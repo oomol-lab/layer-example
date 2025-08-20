47a9d5a391ebde1b24108adc10eca5b6667f76b9
leavesster <11785335+leavesster@users.noreply.github.com>
2025-08-20 15:08:00 +0800

    feat: add task (#2)
    * feat: export task
    
    * update version

---
+++ tasks/a/__init__.py
@@ -0,0 +1,17 @@
+from oocana import Context
+import pandas as pd
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+    input: str
+class Outputs(typing.TypedDict):
+    output: typing.Any
+#endregion
+
+def main(params: Inputs, context: Context) -> Outputs:
+
+    p = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]})
+
+    return {
+      "output": p
+    }
